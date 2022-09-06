924a3b9bb19f7b287bb95d834bc0930ba5e6ad57
bofmar <bofmar@outlook.com>
2022-09-06 10:48:29 +0300

    Place slides next to eachother

--- src/carrousel.js
+++ src/carrousel.js
@@ -62,3 +62,4 @@ export default function carrousel(list) {
 
   return content;
 }
+
--- src/index.js
+++ src/index.js
@@ -7,5 +7,28 @@ import './styles/index.scss';
 
 const images = [p1, p2, p3, p4];
 
+function arangeInRow(images, size) {
+  images.forEach((image, index) => {
+    image.style.left = `${size * index}px`;
+    console.log(image.style.left);
+  });
+}
+
+function activate() {
+  const content = document.querySelector('.carrousel');
+  const bLeft = content.querySelector('.left-button');
+  const bRight = content.querySelector('.right-button');
+  const track = content.querySelector('.carrousel-track');
+  const images = Array.from(content.querySelectorAll('.carrousel-slide'));
+  const indicators = content.querySelectorAll('.carrousel-indicator');
+  const width = images[0].getBoundingClientRect().width;
+  
+  arangeInRow(images,width);
+  bLeft.addEventListener('click', () => {
+    console.log("yo");
+  });
+}
+
 const body = document.querySelector('body');
 body.appendChild(carrousel(images));
+activate();
